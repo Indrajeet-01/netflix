8a2e0a3a6af43053bb3d5e8b216efb5c608bdd4d
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-04-20 21:53:30 +0530

    third

--- src/pages/watch/Watch.jsx
+++ src/pages/watch/Watch.jsx
@@ -8,7 +8,7 @@ function Watch() {
             <ArrowBackOutlined/>
             Home
         </div>
-        <video className='video' autoPlay progress controls src="https://www.videvo.net/video/pair-of-lionesses-walking-together/463057/"  />
+        <video className='video' autoplay progress controls src="https://dsqqu7oxq6o1v.cloudfront.net/motion-array-1296744-Tourist_Jordan_17-high.mp4"  />
     </div>
   )
 }
